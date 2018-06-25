41ac30f88f01f377438a1e0ccc0539a1e7735a3f
Sean Sackowitz <sean.sackowitz@gmail.com>
2018-06-25 14:22:06 -0400

    Added chrome screen sharing options

--- config/jitsi/meet/config.js
+++ config/jitsi/meet/config.js
@@ -10,4 +10,17 @@ var config = { // eslint-disable-line no-unused-vars
     useNicks: false,
     bosh: '//{{ DOMAIN }}/http-bind', // FIXME: use xep-0156 for that
     clientNode: 'http://jitsi.org/jitsimeet', // The name of client node advertised in XEP-0115 'c' stanza
+    
+    // Desktop sharing
+    // The ID of the jidesha extension for Chrome.
+    desktopSharingChromeExtId: '{{ CHROME_EXT_ID }}',
+    // Whether desktop sharing should be disabled on Chrome.
+    desktopSharingChromeDisabled: {{ CHROME_SHARE_ENABLED }},
+    // The media sources to use when using screen sharing with the Chrome
+    // extension.
+    desktopSharingChromeSources: [ 'screen', 'window', 'tab' ],
+    // Required version of Chrome extension
+    desktopSharingChromeMinExtVersion: '0.1',
+    // Whether desktop sharing should be disabled on Firefox.
+    desktopSharingFirefoxDisabled: false,
 };
